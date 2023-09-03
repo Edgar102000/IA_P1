7ec5ee3c7283f6ecffc4a12a3d86ba218401be1b
Edgar102000 <143049716+Edgar102000@users.noreply.github.com>
2023-09-03 12:20:27 -0600

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,4 @@
+comillassim = 'Este es un texto con comillas simples'
+comillasdob = "Este es un texto con comillas dobles"
+frase = '"print()" se utiliza para imprimir valores en la consola '
+print(frase)
