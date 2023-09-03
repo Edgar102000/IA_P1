90bddc78f7131fe3a85d1aec354d73ea90d3e692
Edgar102000 <143049716+Edgar102000@users.noreply.github.com>
2023-09-03 12:22:48 -0600

    Add files via upload

---
+++ Concatenacion.py
@@ -0,0 +1,22 @@
+concatenar = "Asi se concatena " + "una frase"
+print(concatenar)
+
+variable1 = "Asi se concatenan dos "
+variable2 = "variables con Strings"
+variablescon = variable1 + variable2
+print(variablescon)
+
+variableprint = "Asi se concatena una varible "
+print(variableprint + 'en el "print" directamente')
+
+n1 = "Edgar"
+n2 = "Abraham"
+apellido1= "Ruiz"
+apellido2= "Ayala"
+Nombrecomp= n1 + " " + n2 + " " + apellido1 + " " + apellido2
+print(Nombrecomp)
+
+num1 = "20 "
+num2 = "30"
+numr = num1 + num2
+print (numr)
