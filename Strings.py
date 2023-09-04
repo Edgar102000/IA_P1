73791bfa2a0a171754c276cd1121760ad423e8f2
Edgar102000 <143049716+Edgar102000@users.noreply.github.com>
2023-09-03 23:52:12 -0600

    Delete Strings.py

--- Strings.py
+++
@@ -1,4 +0,0 @@
-comillassim = 'Este es un texto con comillas simples'
-comillasdob = "Este es un texto con comillas dobles"
-frase = '"print()" se utiliza para imprimir valores en la consola '
-print(frase)
