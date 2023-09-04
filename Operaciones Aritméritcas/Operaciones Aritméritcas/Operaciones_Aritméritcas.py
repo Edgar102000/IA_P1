33d36d186c825c1ee551e9047ac3c2acbb13f9b5
Edgar102000 <edgar.abraham58@gmail.com>
2023-09-04 00:32:55 -0500

    Operaciones Aritméticas
    Practica 7

---
+++ Operaciones Aritméritcas/Operaciones Aritméritcas/Operaciones_Aritméritcas.py
@@ -0,0 +1,6 @@
+suma = 20 + 23 + 44
+resta = -20 -23 -44
+multiplicacion = (20+23+44)*10
+division = 5000/230/2.17
+operacion = 10 / 5 + 15 - 17
+print ('Suma: ', suma ,'\nResta:', resta, '\nMultiplicacion:',multiplicacion, '\nDivision:', division, '\nOperacion:' ,operacion)
