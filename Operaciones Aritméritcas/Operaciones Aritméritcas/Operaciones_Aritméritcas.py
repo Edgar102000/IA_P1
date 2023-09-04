ca2686d96e820e956221c0faef367c12a6b51f0f
Edgar102000 <143049716+Edgar102000@users.noreply.github.com>
2023-09-03 23:38:26 -0600

    Delete Operaciones Aritméritcas directory

--- Operaciones Aritméritcas/Operaciones Aritméritcas/Operaciones_Aritméritcas.py
+++
@@ -1,6 +0,0 @@
-suma = 20 + 23 + 44
-resta = -20 -23 -44
-multiplicacion = (20+23+44)*10
-division = 5000/230/2.17
-operacion = 10 / 5 + 15 - 17
-print ('Suma: ', suma ,'\nResta:', resta, '\nMultiplicacion:',multiplicacion, '\nDivision:', division, '\nOperacion:' ,operacion)
