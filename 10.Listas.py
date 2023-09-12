e87d337c1cebafe0e5b1f85b6a45a29fa7ea10e3
Edgar102000 <edgar.abraham58@gmail.com>
2023-09-11 23:56:05 -0500

    Practica 10

---
+++ 10.Listas.py
@@ -0,0 +1,3 @@
+print ("El color de la posicion 3 es el verde.\nEl color rojo se encuentra en la posicion 1.\nEl color rosa se encuentra en la posicion 8.")
+numeros =[ 'Tres','Dos','Cinco','Cuatro','Uno']
+print(numeros[0])
