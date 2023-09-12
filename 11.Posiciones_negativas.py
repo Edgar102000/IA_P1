ed648a29834ac77434983669bd4420696ef65729
Edgar102000 <edgar.abraham58@gmail.com>
2023-09-12 18:46:54 -0500

    Practica 11

---
+++ 11.Posiciones_negativas.py
@@ -0,0 +1,11 @@
+colores = ['Rojo', 'Azul', 'Verde', 'Amarillo', 'Marron', 'Lila', 'Negro', 'Rosa', 'Blanco', 'Naranja']
+#naranja
+print(colores [-1])
+#amarillo
+print(colores [-7])
+#Lila
+print(colores[-5])
+#Blanco 
+print(colores[-2])
+#rojo
+print(colores[-10])
