11d74d5245bbf444057a973b338298fdd823bb6f
Edgar102000 <edgar.abraham58@gmail.com>
2023-09-12 19:01:30 -0500

    Practica 12

---
+++ 12.Del.py
@@ -0,0 +1,6 @@
+colores = ['Rojo', 'Azul', 'Verde', 'Amarillo', 'Marron', 'Lila', 'Negro', 'Rosa', 'Blanco', 'Naranja']
+del colores[1]
+del colores[3]
+del colores[-4]
+del colores[-3]
+print(colores)
