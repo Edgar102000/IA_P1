0fbbab0bb65a12cd0817c3445a8889a7fe4ab763
Edgar102000 <edgar.abraham58@gmail.com>
2023-09-15 01:04:09 -0500

    Practica 21

---
+++ 21.Condicionales.py
@@ -0,0 +1,25 @@
+num1 = 15
+num2 = 20
+print ('Los numeros dados son:numero 1=',num1,'y numero 2=',num2,'\nPara que nuestra condicion sea True, tenemos que cambiar el condicional == por menor que, diferente que o menor o igual que.')
+if num1 < num2:
+	print('Se ejecuta el if si el mumero 1 es menor que el numero 2.')
+if num1 != num2:
+	print('Se ejecuta el if si el numero 1 es diferente del numero 2')
+if num1 <= num2:
+	print('Se ejecuta el if si el numero 1 es menor o igual que numero 2.')
+
+num3 = 1450
+num4 = 60
+print ('\nLos numeros dados son:numero 3=',num3,'y numero 4=',num4,'\nPara que nuestra condicion sea True, tenemos que cambiar el condicional < por mayor que, diferente que o mayor o igual que.')
+if num3 > num4:
+	print('Se ejecuta el if si el mumero 3 es mayor que el numero 4.')
+if num3 != num4:
+	print('Se ejecuta el if si el mumero 3 es diferente del numero 4.')
+if num3 >= num4:
+	print('Se ejecuta el if si el mumero 3 es mayor o igual que el numero 4.')
+
+num5 = 1450
+num6 = 1450
+print ('\nLos numeros dados son:numero 5=',num5,'y numero 6= 60\n Para que nuestra condicion sea False, tenemos que cambiar el calor del numero 6 por 1450')
+if num5 != num6:
+	print('Se ejecuta el if.')
