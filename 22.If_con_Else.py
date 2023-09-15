87b07bf3657359c7de7e2bbee2885ea40d363342
Edgar102000 <edgar.abraham58@gmail.com>
2023-09-15 01:25:26 -0500

    Practica 22

---
+++ 22.If_con_Else.py
@@ -0,0 +1,8 @@
+print('Se realizara la correcion de un codigo dado, donde dicha correcion queda funcionando de la siguiente manera:\n')
+
+color = 'rojo'
+if color != 'rojo':
+    print ("El color no es rojo.")
+else:
+   print ("El color es rojo.")
+
