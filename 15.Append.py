fb8f6977b58fab75c292afc1877796906afce2a0
Edgar102000 <edgar.abraham58@gmail.com>
2023-09-14 23:26:14 -0500

    Practica 15

---
+++ 15.Append.py
@@ -0,0 +1,7 @@
+colores = ['Rojo', 'Azul', 'Verde', 'Amarillo', 'Marron', 'Lila', 'Negro', 'Rosa', 'Blanco', 'Naranja']
+fux = ('Fuxia')
+cel = ('Celeste')
+print ('A la siguiente lista:', colores, '\nSe le agregaran los colores:', fux, 'y', cel)
+colores.append(fux)
+colores.append(cel)
+print ('La lista quedaria de la siguiente manera:\n', colores)
