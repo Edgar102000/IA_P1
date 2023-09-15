d5cfe6595cacd3ccdf02ce9514c8304f9bd810ff
Edgar102000 <edgar.abraham58@gmail.com>
2023-09-15 00:30:18 -0500

    Practica 20

---
+++ 20.Lista_en_tupla.py
@@ -0,0 +1,5 @@
+colores = ['Rojo', 'Azul', 'Verde', 'Amarillo', 'Marron', 'Lila', 'Negro', 'Rosa', 'Blanco', 'Naranja']
+tcolores=tuple(colores)
+lcolores=list(tcolores)
+print ('La lista:\n ', colores,'\nSe ha convertido en tupla de la siguiente manera:\n', tcolores,'\nPara corroborarlo, observamos que el tipo de dato es: ',type(tcolores))
+print ('\nA continuacion, cambiaremos nuevamente la tumpla:\n',tcolores,' \nA lista,quedando de la siguiente manera:\n',lcolores,'\nPara comprobarlo, observamos que el tipo de dato es: ',type(lcolores))
