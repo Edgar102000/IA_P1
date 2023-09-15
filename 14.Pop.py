5e3a38598a0f0e9d581f7067e6481b6e244259eb
Edgar102000 <edgar.abraham58@gmail.com>
2023-09-14 23:05:00 -0500

    Practica 14

---
+++ 14.Pop.py
@@ -0,0 +1,4 @@
+colores = ['Rojo', 'Azul', 'Verde', 'Amarillo', 'Marron', 'Lila', 'Negro', 'Rosa', 'Blanco', 'Naranja']
+almacenar_azul = colores.pop(1)
+almacenar_blanco = colores.pop(-2)
+print ('Los colores eliminados son:', almacenar_azul , 'y', almacenar_blanco)
