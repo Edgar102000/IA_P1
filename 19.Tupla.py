592b8c2f0cc0306176bd5df3b53f0e61924593ae
Edgar102000 <edgar.abraham58@gmail.com>
2023-09-15 00:17:55 -0500

    Practica 19

---
+++ 19.Tupla.py
@@ -0,0 +1,5 @@
+colores = ('Rojo', 'Azul', 'Verde', 'Amarillo', 'Marron', 'Lila', 'Negro', 'Rosa', 'Blanco', 'Naranja')
+numeros = (10, 1, 5, 11)
+print ('De la tupla:\n',colores,'\nPodemos decir que en la posicon #2 se encuentra el color: ',colores[1])
+print ('\nEl resultado de cirta operacion utiliznado los numeros de la tupla:', numeros , '\nEs:', numeros[0]+numeros[2]+numeros[3]-numeros[1])
+
