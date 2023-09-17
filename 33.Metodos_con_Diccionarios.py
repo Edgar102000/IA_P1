e911ebb1c1052169219f214be584bda1fd500656
Edgar102000 <edgar.abraham58@gmail.com>
2023-09-17 18:13:03 -0500

    Practica 33

---
+++ 33.Metodos_con_Diccionarios.py
@@ -0,0 +1,22 @@
+teclado1 = {
+	'Categoria': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoria': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+del teclado1
+del teclado2['Categoria']
+del teclado2['Precio']
+print ('Se agregara una nueva clave y calor a teclado2:')
+teclado2['Color']='Negro'
+print(teclado2)
+print(teclado2['Modelo'])
+
+
+
+       
