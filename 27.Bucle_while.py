d11de33df92cb0bb8e0a3e2dfeb637111aafc96b
Edgar102000 <edgar.abraham58@gmail.com>
2023-09-17 17:11:12 -0500

    Practica 27

---
+++ 27.Bucle_while.py
@@ -0,0 +1,17 @@
+print("Este es un blcule hasta que x es igual a 15 con incremento de 5 en 5:")
+x=0
+while x<=15:
+    print(x)
+    x+=5
+    
+print("Este es un bucle hasta que x es igual a -100 con un decremento de 20 en 20:")
+x=0
+while x>=-100:
+    print(x)
+    x-=20
+
+print("Este es un bucle hasta que x es igual a 0 con un decremento de 1 en 1 empezando en el numero 10:")
+x=10
+while x>=0:
+    print(x)
+    x-=1
