ddddc1cc64cd8bac633410b814f96f3c4ff73fbf
Edgar102000 <edgar.abraham58@gmail.com>
2023-09-17 17:54:50 -0500

    Practica 31

---
+++ 31.Diccionario_en_Python.py
@@ -0,0 +1,13 @@
+teclado1 = {
+	'Categoria': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoria': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+print('El modelo es:', teclado2['Modelo'], '\nSu precio es de: $',teclado2['Precio'])
